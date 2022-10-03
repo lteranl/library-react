8e231b2ea9b17b27dedbddfaeafbe7ed688a256a
lteranl <jjteran2018@gmail.com>
2022-10-02 19:49:20 -0700

    filtering out books if theres a sale price, and displaying dynamically

---
+++ src/components/Discounted.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { books } from "../data";
+import Book from "./ui/Book";
+
+function Discounted() {
+    return (
+        <section className="" id="recent">
+            <div className="container">
+                <div className="row">
+                    <div className="section__title">
+                        <h2 className="section__title">
+                            Discounted <span className="purple">Books</span>
+                        </h2>
+                    </div>
+                    <div className="books">
+                        {books
+                            .filter((book) => book.salePrice !== null)
+                            .slice(0, 8)
+                            .map((book) => (
+                                <Book book={book} key={book.id} />
+                            ))}
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Discounted;
