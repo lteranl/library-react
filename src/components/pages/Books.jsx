59881787d742adec4cd45a3dcbb78f4452e8aa39
lteranl <jjteran2018@gmail.com>
2022-10-05 14:00:19 -0700

    implemented sorting options, also displaying all books in the data

---
+++ src/components/pages/Books.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { books } from "../../data";
+import Book from "../ui/Book";
+function Books() {
+    return (
+        <div id="books__body">
+            <main id="books__main">
+                <section>
+                    <div className="books__container">
+                        <div className="row">
+                            <div className="books__header">
+                                <h2 className="section__title books__header--title">
+                                    All Books
+                                </h2>
+                                <select
+                                    name=""
+                                    id="filter"
+                                    defaultValue="DEFAULT"
+                                >
+                                    <option value="DEFAULT" disable="true">
+                                        Sort
+                                    </option>
+                                    <option value="LOW_TO_HIGH">
+                                        Price, Low to High
+                                    </option>
+                                    <option value="HIGH_TO_LOW">
+                                        Price, High to Low
+                                    </option>
+                                    <option value="RATING">Ratings</option>
+                                </select>
+                            </div>
+                            <div className="books">
+                                {books.map((book) => (
+                                    <Book book={book} key={book.id} />
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+        </div>
+    );
+}
+
+export default Books;
