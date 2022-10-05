62caffae568bab004751a79e7042539d8c6c31ad
lteranl <jjteran2018@gmail.com>
2022-10-05 15:24:13 -0700

    imported rating and price component. passing rating, original/sale price as props

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,37 +1,23 @@
 import React from "react";
-import { FaStar, FaStarHalfAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import Price from "./Price";
+import Rating from "./Rating";
 
 function Book({ book }) {
     return (
         <div className="book">
-            <a href="">
+            <Link to="">
                 <figure className="book__img--wrapper">
                     <img src={book.url} alt="" className="book__img" />
                 </figure>
-            </a>
+            </Link>
             <div className="book__title">
-                <a href="" className="book__title--link">
+                <Link to="" className="book__title--link">
                     {book.title}
-                </a>
+                </Link>
             </div>
-            <div className="book__ratings">
-                {new Array(Math.floor(book.rating)).fill(0).map((_, index) => (
-                    <FaStar key={index} />
-                ))}
-                {!Number.isInteger(book.rating) && <FaStarHalfAlt />}
-            </div>
-            <div className="book__price">
-                {book.salePrice ? (
-                    <>
-                        <span className="book__price--normal">
-                            ${book.originalPrice.toFixed(2)}
-                        </span>
-                        ${book.salePrice.toFixed(2)}
-                    </>
-                ) : (
-                    <>${book.originalPrice.toFixed(2)}</>
-                )}
-            </div>
+            <Rating rating={book.rating} />
+            <Price original={book.originalPrice} sale={book.salePrice} />
         </div>
     );
 }
