c35eefd42a7c55ad238a93e83e58af684b8fa615
lteranl <jjteran2018@gmail.com>
2022-10-06 14:44:04 -0700

    simplified addToCart, added second function to handle quantity change

--- src/App.js
+++ src/App.js
@@ -12,29 +12,21 @@ function App() {
     const [cart, setCart] = useState([]);
 
     function addToCart(book) {
-        // + converts string to number (unary operator) - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Arithmetic_Operators#Unary_plus
-        const dupeItem = cart.find((item) => +item.id === +book.id);
-        if (dupeItem) {
-            // increment quantity
-            dupeItem.quantity++;
-            setCart(
-                cart.map((item) => {
-                    // if item is the dupeItem then return the dupeItem with the incremented quantity
-                    if (item.id === dupeItem.id) {
-                        return {
-                            ...item,
-                            quantity: item.quantity++,
-                        };
-                    } else {
-                        // otherwise return the item
-                        return item;
-                    }
-                })
-            );
-        } else {
-            // if there is no dupeItem then add the book to the cart with a quantity of 1
-            setCart([...cart, { ...book, quantity: 1 }]);
-        }
+        setCart([...cart, { ...book, quantity: 1 }]);
+    }
+
+    function changeQuantity(book, quantity) {
+        setCart(
+            // map() returns a new array with the same length as the original array and with the same elements in the same order
+            cart.map((item) =>
+                // if the id of the item in the cart matches the id of the book that was passed in
+                item.id === book.id
+                    ? // return a new object with the same properties as the item in the cart, but with the quantity property set to the quantity that was passed in
+                      { ...item, quantity: +quantity }
+                    : // otherwise, return the item in the cart
+                      item
+            )
+        );
     }
 
     console.log(cart);
@@ -49,12 +41,22 @@ function App() {
                     <Route
                         path="/books/:id"
                         element={
-                            <BookInfo books={books} addToCart={addToCart} />
+                            <BookInfo
+                                books={books}
+                                addToCart={addToCart}
+                                cart={cart}
+                            />
                         }
                     />
                     <Route
                         path="/cart"
-                        element={<Cart books={books} cart={cart} />}
+                        element={
+                            <Cart
+                                books={books}
+                                cart={cart}
+                                changeQuantity={changeQuantity}
+                            />
+                        }
                     />
                 </Routes>
                 <Footer />
